e85044a5b160499108834f23a2d6fa7186669dc4
gbatista321 <goncalo.m.o.mesquita@tecnico.ulisboa.pt>
2018-05-11 02:40:38 +0100

    fix tests

--- Client/src/test/java/pt/ulisboa/tecnico/hdscoin/ClientTests/ByzantineServer.java
+++ Client/src/test/java/pt/ulisboa/tecnico/hdscoin/ClientTests/ByzantineServer.java
@@ -3,12 +3,15 @@ package pt.ulisboa.tecnico.hdscoin.ClientTests;
 import static org.junit.Assert.assertTrue;
 
 import java.net.MalformedURLException;
+import java.rmi.AlreadyBoundException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 
 import org.junit.Test;
 
 import pt.ulisboa.tecnico.hdscoin.client.Client;
+import pt.ulisboa.tecnico.hdscoin.server.NoReplyServer;
+
 import static org.junit.Assert.assertTrue;
 
 import java.net.MalformedURLException;
@@ -46,4 +49,31 @@ public class ByzantineServer {
 		alice.setServerByzantine(false);
 		
 	}
+	@Test
+	public void noReplyAttack() {
+		try {
+ 			alice=new Client("localhost", "alice", "alice123",false,0);
+			NoReplyServer sv = new NoReplyServer(1, 4);
+			alice.setByzantineServer(0, sv);
+ 			assertTrue(alice.register());
+ 			
+ 		} catch (RemoteException e) {
+ 			// TODO Auto-generated catch block
+ 			e.printStackTrace();
+ 		} catch (MalformedURLException e) {
+ 			// TODO Auto-generated catch block
+ 			e.printStackTrace();
+ 		} catch (NotBoundException e) {
+ 			// TODO Auto-generated catch block
+ 			e.printStackTrace();
+ 		} catch (AlreadyBoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertTrue(alice.check("alice"));
+		assertTrue(alice.send("bob", "1"));
+ 		
+ 	
+	}
+	
 }
